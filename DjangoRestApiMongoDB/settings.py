b5e77b1959d2c1fa44dfcb084315bd3eaa41be4d
Deskola <denisogunde@gmail.com>
2020-12-11 03:01:39 +0300

    Deployment cloud

--- DjangoRestApiMongoDB/settings.py
+++ DjangoRestApiMongoDB/settings.py
@@ -138,7 +138,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #Cross Origin Header
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:8081',
-# )
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DjangoRestApiMongo.wsgi --log-file -
